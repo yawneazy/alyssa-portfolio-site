95b4e033eebbc5e6e902cb595699d81a75a7a20b
yaneza@email.arizona.edu <yaneza@email.arizona.edu>
2026-04-20 21:06:54 -0700

    navbar & hamburger menu updates/changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import {FaGithub, FaLinkedin} from  "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import { SiBuymeacoffee } from "react-icons/si";
 import '../styles/Navbar.css';
 
 const Navbar = () => {
-  const [menuClass, setMenuClass] = useState("menu hidden");
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+    const [menuClass, setMenuClass] = useState("menu hidden");
   const [burgerClass, setBurgerClass] = useState("");
   const [isMenuClicked, setIsMenuClicked] = useState(false);
-  const location = useLocation();
 
-  const toggleMenu = () => {
+    const toggleMenu = () => {
     if (!isMenuClicked) {
       setBurgerClass("close");
       setMenuClass("menu visible");
@@ -21,10 +23,12 @@ const Navbar = () => {
     setIsMenuClicked(!isMenuClicked);
   };
 
-  const handleLinkClick = () => {
-    setBurgerClass("");
-    setMenuClass("menu hidden");
-    setIsMenuClicked(false);
+    const handleLinkClick = () => {
+    if (isMenuClicked) {
+      setBurgerClass("");
+      setMenuClass("menu hidden");
+      setIsMenuClicked(false);
+    }
   };
 
   useEffect(() => {
@@ -33,7 +37,7 @@ const Navbar = () => {
 
   const navLinks = (
     <>
-  <Link to="/" onClick={handleLinkClick}>Home</Link>
+      <Link to="/" onClick={handleLinkClick}>Home</Link>
       <Link to="/work" onClick={handleLinkClick}>Work</Link>
       <Link to="/about" onClick={handleLinkClick}>About</Link>
 
@@ -53,110 +57,44 @@ const Navbar = () => {
       <a href="https://github.com/yawneazy" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-  
-      <a href="mailto:your@email.com">
+      <a href="mailto:alyssa.kyanez1@gmail.com">
         <IoIosMail />
       </a>
-  
-      <a
-        href="https://www.linkedin.com/in/alyssayanezolson"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href="https://www.linkedin.com/in/alyssayanezolson" target="_blank" rel="noreferrer">
         <FaLinkedin />
+      </a>
+      <a href="https://buymeacoffee.com/yawneazy" target="_blank" rel="noreferrer">
+        <SiBuymeacoffee />
       </a>
     </div>
   );
 
   return (
     <nav className="navbar">
-      <div className={`burger-menu ${burgerClass}`} onClick={toggleMenu}>
-        <div className="burger-bar"></div>
-        <div className="burger-bar"></div>
-        <div className="burger-bar"></div>
-      </div>
 
-      <Link to="/" className="brand">AY.OLSON</Link>
+      <Link to="/" className="brand">ALYSSA OLSON</Link>
 
-      {/* DESKTOP */}
+      {/* BURGER (mobile only) */}
+      <div className={`burger-menu ${isMenuClicked ? "open" : ""}`} onClick={toggleMenu}>
+  <div className="burger-bar"></div>
+  <div className="burger-bar"></div>
+  <div className="burger-bar"></div>
+</div>
+
+      {/* DESKTOP NAV */}
       <div className="nav-links desktop-nav">
         {navLinks}
         {socialLinks}
       </div>
 
-      {/* MOBILE */}
-      <div className={`${menuClass} nav-links mobile-nav`}>
+      {/* MOBILE NAV */}
+      <div className={`mobile-nav ${isMenuClicked ? "visible" : "hidden"}`}>
         {navLinks}
+        {socialLinks}
       </div>
+
     </nav>
   );
 };
 
 export default Navbar;
-// import React, { useState, useEffect } from 'react';
-// import { Link, useLocation } from 'react-router-dom';
-// import '../styles/Navbar.css';
-
-
-// const Navbar = () => {
-//   const [menuClass, setMenuClass] = useState("menu hidden");
-//   const [burgerClass, setBurgerClass] = useState("");
-//   const [isMenuClicked, setIsMenuClicked] = useState(false);
-//   const location = useLocation();
-
-//   const toggleMenu = () => {
-//     if (!isMenuClicked) {
-//       setBurgerClass("close");
-//       setMenuClass("menu visible");
-//     } else {
-//       setBurgerClass("");
-//       setMenuClass("menu hidden");
-//     }
-//     setIsMenuClicked(!isMenuClicked);
-//   };
-
-//   const handleLinkClick = () => {
-//     if (isMenuClicked) {
-//       setBurgerClass("");
-//       setMenuClass("menu hidden");
-//       setIsMenuClicked(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     handleLinkClick();
-//   }, [location]);
-
-//   return (
-//     <nav className="navbar">
-//       <div className={`burger-menu ${burgerClass}`} onClick={toggleMenu}>
-//         <div className="burger-bar"></div>
-//         <div className="burger-bar"></div>
-//         <div className="burger-bar"></div>
-//       </div>
-//       <Link to="/" className="brand">ALYSSA OLSON</Link>
-//       <div className="nav-links">
-//         <Link to="/work">Work</Link>
-//         {/* <Link to="/design">Design</Link> */}
-//         {/* <Link to="/play">Play</Link> */}
-//         <Link to="/about">About</Link>
-
-//         <a href="/docs/A.Olson_Resume.pdf" download className="resume-btn">
-//     Resume
-//   </a>
-//       </div>
-//       <div className={`${menuClass} nav-links`}>
-//         <Link to="/work">Work</Link>
-//         {/* <Link to="/design">Design</Link> */}
-//         {/* <Link to="/play">Play</Link> */}
-//         <Link to="/about">About</Link>
-
-//         <a href="/docs/A.Olson_Resume.pdf" download className="resume-btn">
-//     Resume
-//   </a>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
